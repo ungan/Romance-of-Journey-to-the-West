165036748586185a12131ccb24c626795715053e
ungan <83686779+ungan@users.noreply.github.com>
2022-07-27 16:56:40 +0900

    a
    a

--- Assets/6. Scripts/attack_range.cs
+++ Assets/6. Scripts/attack_range.cs
@@ -4,17 +4,16 @@ using UnityEngine;
 
 public class attack_range : MonoBehaviour
 {
+   
     public Enemy_ai enemy_ai;
     // Start is called before the first frame update
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        
-        if(collision.gameObject.tag == "Leader")
+
+        if (collision.gameObject.tag == "Leader")
         {
-            enemy_ai.enemy_state = e_state.attack_ready;      // attack range 안에 player 감지시 
-            enemy_ai.player = collision.gameObject;
-            enemy_ai.move_false();
+            enemy_ai.inrange = true;
         }
 
     }
@@ -23,21 +22,19 @@ public class attack_range : MonoBehaviour
     {
         if (collision.gameObject.tag == "Leader")
         {
-            enemy_ai.enemy_state = e_state.Follow;      // attack range 밖으로 player 벗어남
-            enemy_ai.move_true();
-            enemy_ai.player = null;
+            enemy_ai.inrange = false;
         }
-       
+
     }
 
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
